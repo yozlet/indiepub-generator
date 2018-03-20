9bb321067c090150aad9eba515e1cc21624016a6
Yoz (Jeremy) Grahame <jeremy.grahame@gsa.gov>
2018-03-19 19:03:37 -0700

    initial commit

---
+++ grammar.js
@@ -0,0 +1,35 @@
+/**
+ * @author Yoz & co.
+ */
+
+var grammar = {
+
+  clothesAdjective: [
+    "Tight",
+    "Skinny",
+    "Velvet",
+    "Linen",
+    "Black",
+    "Skimpy",
+    "Denim",
+    "Baggy",
+    "Sparkly",
+    
+  ],
+
+  designElement: [
+    "Waterfall",
+    "Aspidistra",
+    "Skylight",
+    "Awning",
+    "Birch",
+    "Rockface",
+    "Seaglass",
+
+  ],
+
+  origin : [
+    "#clothesAdjective# #designElement#"
+  ]    
+
+}
